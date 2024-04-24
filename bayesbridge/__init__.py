37ae1d3e6f14463a24c53e745e4c6c0c88b2245b
YuzhengDun1999 <ydun2@jhu.edu>
2024-04-23 23:27:49 -0400

    fix __init__.py

--- bayesbridge/__init__.py
+++ bayesbridge/__init__.py
@@ -1,6 +1,4 @@
-from .bayesbridge import BayesBridge
 from .prsbridge import PrsBridge
 from .gibbs_util import SamplerOptions
 from .prior import RegressionCoefPrior
 from .model import PRSModel
-from .model import RegressionModel
